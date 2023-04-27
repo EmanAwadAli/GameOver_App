309ad4fd8f43b20c21d6d737c063d10070903612
Eman <emanwork369@gmail.com>
2023-04-27 17:58:16 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "GameOver_App",
+  base: "/GameOver_App",
   plugins: [react()],
 });
